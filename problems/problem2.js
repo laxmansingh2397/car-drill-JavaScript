be3b21d90b21fe21e8fd14de75ce16561908ef75
Laxman Singh <singhlaxman2397@gmail.com>
2025-08-20 14:25:43 +0530

    Solved problem2

---
+++ problems/problem2.js
@@ -0,0 +1,12 @@
+// ==== Problem #2 ====
+// The dealer needs the information on the last car in their inventory. Execute a function to find what the make and model of the last car in the inventory is?  Log the make and model into the console in the format of:
+// "Last car is a *car make goes here* *car model goes here*"
+
+
+function problem2(inventory) {
+    const carData = inventory[inventory.length - 1]
+    return `Last car is a ${carData["car_make"]} ${carData["car_model"]}`
+}
+
+
+module.exports = problem2;
